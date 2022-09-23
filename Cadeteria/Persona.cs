daac7e034ae94bb8f7f26c9ae119d0585d47bf12
emybarr <eybarreiro.2a@gmail.com>
2022-09-23 18:57:09 -0300

    inicio

---
+++ Cadeteria/Persona.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadeteria
+{
+    public abstract class Persona
+    {
+        private int id;
+        private string nombre;
+        private string direccion;
+        private int telefono; 
+        
+    }
+}
---
+++ Cadeteria/pedidos.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadeteria
+{
+    public class pedidos
+    {
+        private int numero;
+        private string observaciones;
+        Cliente cliente;
+        private string estado;
+        
+
+    }
+}
